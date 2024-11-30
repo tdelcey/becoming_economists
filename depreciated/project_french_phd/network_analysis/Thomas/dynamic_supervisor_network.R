792cacdd4b1691cf880327fca16f4cf15488bb03
agoutsmedt <agoutsmedt@hotmail.fr>
2024-11-30 15:34:01 +0100

    removing depreciated from public

--- depreciated/project_french_phd/network_analysis/Thomas/dynamic_supervisor_network.R
+++
@@ -1,8 +0,0 @@
-source(here::here("0_paths_and_packages.R"))
-
-thesis_table <- readRDS(here(FR_cleaned_data_path, "all_thesis_table.rds"))
-people_table <- readRDS(here(FR_cleaned_data_path, "people_table.rds"))
-
-
-### preparing data: creating edges and nodes ###
-
